bb585c96db808b9096fcbe2e608be6c9d509d64d
bokney <toknyk@gmail.com>
2024-07-12 09:44:58 +0100

    moved connection and seed files

---
+++ db/connection.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+import pg8000.native
+from db.config import database, password, user
+
+
+load_dotenv(override=True)
+
+
+def connect_to_db():
+    return pg8000.native.Connection(
+        user=user,
+        password=password,
+        database=database,
+        # host=os.getenv("PG_HOST"),
+        # port=int(os.getenv("PG_PORT"))
+    )
+
+
+def close_db_connection(conn):
+    conn.close()
---
+++ db/seed.py
@@ -0,0 +1,17 @@
+from db.connection import connect_to_db, close_db_connection
+
+def seed():
+    print('opening db connection')
+    db = connect_to_db()
+    db.run("DROP TABLE IF EXISTS items;")
+    db.run("""
+        CREATE TABLE items(
+            item_id SERIAL PRIMARY KEY,
+            name VARCHAR(128) NOT NULL,
+            description VARCHAR(512) NOT NULL
+        );
+    """)
+    print('closing db connection')
+    close_db_connection(db)
+
+seed()
